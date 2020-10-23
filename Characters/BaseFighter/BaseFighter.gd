2685f0d685670477dce2992a401efab8a46709fa
marrero171 <50031007+marrero171@users.noreply.github.com>
2020-10-23 00:51:36 -0400

    Improved stats code

--- Characters/BaseFighter/BaseFighter.gd
+++ Characters/BaseFighter/BaseFighter.gd
@@ -8,27 +8,31 @@ onready var baseFSM = $BaseFSM
 var velocity = Vector2()
 var max_speed = 580
 var max_strength = 100
-var max_defence = 100
+var max_defense = 100
 var speed
 var strength
-var defence
+var defense
 var jump_velocity = -620
 var min_jump_velocity = -200
 var is_grounded = false
 
+export var stats = {
+	"speed" : 0, #make it 1-10
+	"strength" : 0, #same as above, make it 1-10
+	"defense" : 0
+}
+
 #these "can" be negative but not zero
 
-export (int) var speed_stat #make it 1-10
-export (int) var strength_stat #same as above make it 1-10 
-export (int) var defence_stat
 
 
 func error_start_check():
 	#returns false if is missing key components
-	if speed_stat == 0 ||strength_stat == 0 ||defence_stat == 0:
-		print("stats can not be zero!")
-		return false
-	elif !ground_raycasts:
+	for i in stats.values():
+		if i == 0:
+			print("stats can not be zero!")
+			return false
+	if !ground_raycasts:
 		print("You must add ground raycasts to player. Use Node2d named 'GroundRaycasts' with raycast2ds as children")
 	elif !body:
 		print("Please Store all visual nodes such as sprites inside a Node2D named 'Body'")
@@ -41,9 +45,9 @@ func error_start_check():
 func _ready():
 	set_physics_process(false)
 	if error_start_check():
-		speed = max_speed * speed_stat/10
-		strength = max_strength*strength_stat/10
-		defence = max_defence*defence_stat/10
+		stats.speed = max_speed * stats.speed / 10
+		stats.strength = max_strength * stats.strength / 10
+		stats.defense = max_defense * stats.defense / 10
 		set_physics_process(true)
 	else:
 		get_tree().quit()
@@ -90,7 +94,7 @@ func _physics_process(delta):
 
 func _handle_sideways_movement():
 	var move_direction = -int(Input.is_action_pressed("player_left"))+int(Input.is_action_pressed("player_right"))
-	velocity.x = lerp(velocity.x,speed*move_direction,_get_h_weight())
+	velocity.x = lerp(velocity.x, stats.speed * move_direction,_get_h_weight())
 	if move_direction != 0:
 		body.scale.x = move_direction
 
